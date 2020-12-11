15efc6b147341ea3e5e0aaabe139cac2ee1a2911
Riyaz Ali <54612827+aayrm5@users.noreply.github.com>
2020-12-11 13:45:48 +0530

    assignment4_p2 completed

--- Assignments/assignment4.py
+++ Assignments/assignment4.py
@@ -1,10 +1,13 @@
 import numpy as np
 
-#Instead of JupyterNotebook, this assignment is gonna be in python script, brace yourselves.
+#Instead of Jupyter Notebook, this assignment is gonna be in python script, brace yourselves.
 
-#Write a Python Program(with class concepts) to find the area of the triangle using the below formula. 
-# area = (s*(s-a)(s-b)(s-c)) ** 0.5 
-# Function to take the length of the sides of triangle from user should be defined in the parent class and function to calculate the area should be defined in subclass.
+# Problem 1
+"""Write a Python Program(with class concepts) to find the area of the triangle using the below formula. 
+area = (s*(s-a)(s-b)(s-c)) ** 0.5 
+Function to take the length of the sides of triangle from user should be defined in the parent class and 
+function to calculate the area should be defined in subclass.
+"""
 
 class Triangle:
     def __init__(self):
@@ -48,3 +51,35 @@ Area_of_Triangle.area_of_triangle()
 #o/p: The sides of triangle are a=35.0, b=24.0, c=26.0
 # The Area of the triangle is 48649.22
 
+# Problem 2
+"""Write a function filter_long_words() that takes a list of words 
+ and an integer n and returns the list of words that are longer than n
+"""
+def filter_long_words(WordList, n):
+    #returns list of words that are longer than Arguement n
+    #n has been considered an integer
+    WordList = [x.strip() for x in WordList if len(x.strip()) > n]
+    
+    if (len(WordList)>0):
+        print("The list of words that are longer than "+str(n)+" is :")
+        return WordList
+    else:
+        return "No words longer than specified number"
+    
+WordList_eg = list(input("Enter the list of words: ").split(','))
+number = int(input("Enter your number :"))
+
+#Function Calling
+filter_long_words(WordList_eg, number)
+
+"""Input:
+Please enter your words: 'City', 'Charminar', 'India', 'Hyderabad', 'Eiffel Tower', 'Pari'
+Please enter your number: 4
+
+Output:
+['Charminar', 'India', 'Hyderabad', 'Eiffel Tower']
+"""
+# Problem 3
+"""Write a Python program using function concept that maps list of words into a list of integers representing the lengths of the corresponding words . 
+Hint: If a list [ ab,cde,erty] is passed on to the python function output should come as [2,3,4] Here 2,3 and 4 are the lengths of the words in the list.
+"""
